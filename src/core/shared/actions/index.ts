7fe446b6839fbc7d0f0e1519f9562390ca9e4025
阮志雄 <770304867@qq.com>
2023-09-10 22:37:09 +0800

    perf： 代码格式化

--- src/core/shared/actions/index.ts
+++ src/core/shared/actions/index.ts
@@ -1,8 +1,12 @@
 export class Actions {
   name = 'default.actions'
   private currentId: string | null = null
+  private actions: any[]
   public executeAction(action: { componentId: string; actionType: string }) {
     this.currentId = action.componentId
   }
   public updateComponent() {}
+  public use(action) {
+    this.actions.push(action)
+  }
 }
