fff3f00c18bc9da1a19721f59ef8b0e2adfab367
rzx100 <770304867@qq.com>
2023-09-11 17:58:36 +0800

    feat: 组件事件交互

--- src/core/shared/actions/index.ts
+++ src/core/shared/actions/index.ts
@@ -1,12 +1,22 @@
+export type Taction = { componentId: string; actionType: string }
+export type TactionFn = {
+  actionType: string
+  callback: (arg: Taction) => void
+}
 export class Actions {
   name = 'default.actions'
   private currentId: string | null = null
-  private actions: any[]
-  public executeAction(action: { componentId: string; actionType: string }) {
+  private actions: TactionFn[] = []
+  public executeAction(action: Taction) {
     this.currentId = action.componentId
   }
-  public updateComponent() {}
-  public use(action) {
-    this.actions.push(action)
+  public updateComponent() { }
+  public use(actionFn: TactionFn) {
+    this.actions.push(actionFn)
+  }
+  public async execute(action: Taction) {
+    for await (const item of this.actions) {
+      action.actionType === item.actionType && item.callback(action)
+    }
   }
 }
